820d1afe122cc1948569c1b65b70dd6ac12be6c6
Jeff Lai <jefflai333@gmail.com>
2018-05-26 15:24:50 -0400

    Edits

---
+++ CSVReader.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+
+	String csvFile = "./Amazon.csv";
+	BufferedReader br = null;
+	String line = "";
+	String cvsSplitBy = ",";
+
+	try {
+
+	    br = new BufferedReader(new FileReader(csvFile));
+	    while ((line = br.readLine()) != null) {
+
+		// use comma as separator
+		String[] country = line.split(cvsSplitBy);
+
+		System.out.println("Country [code= " + country[0] + " , name=" + country[1] + "]");
+
+	    }
+
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (br != null) {
+		try {
+		    br.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+
+    }
+
+}
